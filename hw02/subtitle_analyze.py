499468c5faa329fb27d9a3a3310292e5902fc618
gZeyu <gzy8535@gmail.com>
2018-03-20 08:43:09 +0800

    hw02 beta0.01

--- hw02/subtitle_analyze.py
+++ hw02/subtitle_analyze.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
 import re
+import timeit
 
 
-def time_format(time_string):
+def format_time(time_string):
 
     elements = re.split(r'[:,]', time_string)
     hours = int(elements[0])
@@ -15,26 +16,48 @@ def time_format(time_string):
     return time_stamp
 
 
+def segment_word(text):
+
+    pattern = r'''
+        (?x)                   # set flag to allow verbose regexps
+        (?:[A-Z]\.)+           # abbreviations, e.g. U.S.A.
+        |\d+(?:\.\d+)?%?       # numbers, incl. currency and percentages
+        |\w+(?:[-']\w+)*       # words w/ optional internal hyphens/apostrophe
+        # |(?:[+/\-@&*])         # special characters with meanings
+        # |\S\w*                 # any sequence of word characters#
+        # |[][{}.,;"'?():_`-]    # these are separate tokens
+        '''
+    word_list = re.findall(pattern, text)
+    return word_list
+
+
 def read_subtitle_file(filename):
 
     milliseconds = 0
+    word_count = 0
+
     with open(filename, 'r') as file:
         text = file.readlines()
         for i in range(len(text)):
             if '-->' in text[i]:
                 elements = text[i].split()
-                milliseconds += time_format(elements[2]) - time_format(
+                milliseconds += format_time(elements[2]) - format_time(
                     elements[0])
-                print(text[i])
-        # for line in text:
-        #     if '-->' in line:
-        #         elements = line.split()
-        #         milliseconds += time_format(elements[2]) - time_format(
-        #             elements[0])
-        #         print(line)
+                word_count += len(segment_word(text[i + 1]))
+    frequency = word_count / (milliseconds / 60000)
+
+    # print(word_count)
+    # print(milliseconds / 60000)
+    return frequency
+
+
+def func():
+    read_subtitle_file('discoverycuriositys01e06.eng.srt')
 
 
 if __name__ == '__main__':
 
-    read_subtitle_file('discoverycuriositys01e06.eng.srt')
-    # print(time_format('00:01:59,930'))
+    t = timeit.repeat(
+        'func()', 'from __main__ import func', number=1, repeat=5)
+    print(t)
+    print(min(t))
