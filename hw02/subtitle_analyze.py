4a2a892f364130142548d55135ad87fbd32e5982
gZeyu <gzy8535@gmail.com>
2018-03-21 14:05:36 +0800

    console beta 0.1.0

--- hw02/subtitle_analyze.py
+++ hw02/subtitle_analyze.py
@@ -3,12 +3,15 @@ import sys
 import os
 import re
 import concurrent.futures
+import threading
 import timeit
+import time
 import math
+import random
+import queue
 
 
 def format_time(time_string):
-
     elements = re.split(r'[:,]', time_string)
     hours = int(elements[0])
     minutes = int(elements[1])
@@ -19,7 +22,6 @@ def format_time(time_string):
 
 
 def segment_word(text):
-
     pattern = r'''
         (?x)                   # set flag to allow verbose regexps
         (?:[A-Z]\.)+           # abbreviations, e.g. U.S.A.
@@ -34,7 +36,6 @@ def segment_word(text):
 
 
 def read_subtitle_file(filename):
-
     milliseconds = 0
     word_count = 0
     with open(filename, 'r') as file:
@@ -45,12 +46,13 @@ def read_subtitle_file(filename):
                 milliseconds += format_time(elements[2]) - format_time(
                     elements[0])
                 word_count += len(segment_word(text[i + 1]))
+                # print(text[i + 1])
     frequency = word_count / (milliseconds / 60000)
+    # print((milliseconds / 60000))
     return frequency
 
 
 def get_subtitle_filename_list(path):
-
     filename_list = list()
     for file in os.listdir(path):
         filename = os.path.join(path, file)
@@ -60,9 +62,6 @@ def get_subtitle_filename_list(path):
 
 
 def assign_tasks(filename_list, grain_size=1):
-
-    # task_list = [[] for x in range(math.ceil(len(filename_list) / grain_size))]
-
     task_list = [[
         filename_list[x * grain_size + y] for y in range(grain_size)
         if x * grain_size + y < len(filename_list)
@@ -70,8 +69,46 @@ def assign_tasks(filename_list, grain_size=1):
     return task_list
 
 
-def task(filename_list):
+def execute_task_1(task_queue, result_queue):
+    while True:
+        try:
+            filename_list = task_queue.get()
+            result = result_queue.get()
+            for filename in filename_list:
+                frequency = read_subtitle_file(filename)
+                result[filename] = frequency
+            result_queue.put(result)
+        finally:
+            task_queue.task_done()
+            result_queue.task_done()
 
+
+def multithread_analyze_1(filename_list, max_workers=1, grain_size=1):
+    task_list = assign_tasks(filename_list, grain_size)
+    task_queue = queue.Queue()
+    result_queue = queue.Queue()
+    for task in task_list:
+        task_queue.put(task)
+    result_queue.put(dict())
+    thread_list = [
+        threading.Thread(
+            target=execute_task_1, args=(
+                task_queue,
+                result_queue, )) for x in range(max_workers)
+    ]
+    for thread in thread_list:
+        thread.daemon = True
+        thread.start()
+    try:
+        task_queue.join()
+    except KeyboardInterrupt:
+        print("stopped by hand")
+    while result_queue.empty() == False:
+        return result_queue.get()
+    return dict()
+
+
+def single_thread_analyze(filename_list):
     result = dict()
     for filename in filename_list:
         frequency = read_subtitle_file(filename)
@@ -79,27 +116,42 @@ def task(filename_list):
     return result
 
 
-def multithread_analyze(filename_list, max_workers=1, grain_size = 1):
+def output_analysis_result_document(result_dict,
+                                    filename='analysis_result.txt'):
+    result_list = sorted(result_dict.items(), key=lambda x: x[1])
+    with open(filename, 'w') as file:
+        for result in result_list:
+            file.writelines('%s\t%f\n' % (result[0], result[1]))
 
-# pool = concurrent.futures.ThreadPoolExecutor(max_workers=4)
-# future1 = pool.submit(task, ("hello"))  # 往线程池里面加入一个task
-# future2 = pool.submit(task, ("world"))  # 往线程池里面加入一个task
-# print(future1.done())  # 判断task1是否结束
-# time.sleep(3)
-# print(future2.done())  # 判断task2是否结束
-# print(future1.result())  # 查看task1返回的结果
-# print(future2.result())  # 查看task2返回的结果
 
-# def func():
+def test_multithread_analyze_1():
+    filename_list = get_subtitle_filename_list('./srt')
+    result_dict = multithread_analyze_1(
+        filename_list, max_workers=2, grain_size=4)
+    output_analysis_result_document(
+        result_dict, filename='analysis_result.txt')
+    # read_subtitle_file('./srt/bbc戴维阿滕伯勒非洲s01e04.eng.srt')
 
-#     read_subtitle_file('discoverycuriositys01e06.eng.srt')
+
+def test_single_thread_analyze():
+    filename_list = get_subtitle_filename_list('./srt')
+    result_dict = single_thread_analyze(filename_list)
+    output_analysis_result_document(
+        result_dict, filename='analysis_result.txt')
+
 
 if __name__ == '__main__':
 
-    # t = timeit.repeat(
-    #     'func()', 'from __main__ import func', number=1, repeat=5)
-    # print(t)
-    # print(min(t))
-    filename_list = get_subtitle_filename_list('./srt')
-    # multithread_analyze(filename_list)
-    task_list = assign_tasks(filename_list, grain_size=5)
+    t = timeit.repeat(
+        'test_multithread_analyze_1()',
+        'from __main__ import test_multithread_analyze_1',
+        number=20,
+        repeat=1)
+    print('Average time : %f, Minimum time : %f' % (sum(t) / len(t), min(t)))
+
+    t = timeit.repeat(
+        'test_single_thread_analyze()',
+        'from __main__ import test_single_thread_analyze',
+        number=20,
+        repeat=1)
+    print('Average time : %f, Minimum time : %f' % (sum(t) / len(t), min(t)))
--- multithread_demo.py
+++ multithread_demo.py
@@ -7,11 +7,13 @@ import queue
 
 def read(q):
     while True:
-        if q.empty():
-            break
-        value = q.get()
-        print('Get %s from queue.' % value + threading.current_thread().name)
-        time.sleep(random.random())
+        try:
+            value = q.get()
+            print('Get %s from queue.' % value +
+                  threading.current_thread().name)
+            time.sleep(random.random())
+        finally:
+            q.task_done()
 
 
 def main():
@@ -20,12 +22,15 @@ def main():
     pw2 = threading.Thread(target=read, args=(q, ))
     pw1.daemon = True
     pw2.daemon = True
-    for c in [chr(ord('A') + i) for i in range(26)]:
-        q.put(c)
     pw1.start()
     pw2.start()
+    for c in [chr(ord('A') + i) for i in range(26)]:
+        q.put(c)
+    try:
+        q.join()
+    except KeyboardInterrupt:
+        print("stopped by hand")
 
 
 if __name__ == '__main__':
     main()
-    print("end")
