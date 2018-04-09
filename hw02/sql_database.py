503871bb0e5ba30c448fa944c322b1da3e4015b2
gZeyu <gzy8535@gmail.com>
2018-04-09 23:14:58 +0800

    sort func

--- hw02/sql_database.py
+++ hw02/sql_database.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import (Qt, QSortFilterProxyModel)
+from PyQt5.QtCore import (Qt)
 from PyQt5.QtWidgets import QApplication, QTableView, QMessageBox
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
 
@@ -16,8 +16,8 @@ def create_connection():
     query.exec_("create table subtitle(id int primary key, "
                 "filename varchar(20), count int, time int, frequency int)")
     query.exec_("insert into subtitle values(1, 'test.srt', 100, 200, 100)")
-    query.exec_("insert into subtitle values(2, 'test.srt', 200, 300, 100)")
-    query.exec_("insert into subtitle values(3, 'test.srt', 300, 100, 100)")
+    query.exec_("insert into subtitle values(2, 'sdfsda.srt', 200, 300, 100)")
+    query.exec_("insert into subtitle values(3, 'rgrd.srt', 300, 100, 100)")
     return True
 
 
@@ -39,6 +39,7 @@ def create_view(title, model):
     view = QTableView()
     view.setModel(model)
     view.setWindowTitle(title)
+    view.setSortingEnabled(True)
     return view
 
 
@@ -53,10 +54,8 @@ if __name__ == '__main__':
     model = QSqlTableModel()
     initialize_model(model)
 
-    sql_proxy = QSortFilterProxyModel()
-    sql_proxy.setSourceModel(model)
-    view1 = create_view("Table Model (View 1)", sql_proxy)
-    view2 = create_view("Table Model (View 2)", sql_proxy)
+    view1 = create_view("Table Model (View 1)", model)
+    view2 = create_view("Table Model (View 2)", model)
 
     view1.show()
     view2.move(view1.x() + view1.width() + 20, view1.y())
