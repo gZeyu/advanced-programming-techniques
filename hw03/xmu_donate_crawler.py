960d46dc3d563bb8ea4f6b2f708fdd3259423898
gZeyu <gzy8535@gmail.com>
2018-04-25 03:10:43 +0800

    hw03 dev 0.3.0 : download completed

--- hw03/xmu_donate_crawler.py
+++ hw03/xmu_donate_crawler.py
@@ -7,6 +7,7 @@ import datetime
 from urllib import (request, error)
 import queue
 import threading
+import time
 
 
 def init_logger():
@@ -33,7 +34,7 @@ def get_html(url):
     logger = logging.getLogger(__file__)
     html = None
     try:
-        html = request.urlopen(url, timeout=3)
+        html = request.urlopen(url, timeout=5)
     except Exception:
         logger.error(traceback.format_exc())
 
@@ -47,7 +48,7 @@ def save_html(str, filepath):
 
 def download(start=1, stop=10):
     logger = logging.getLogger(__file__)
-    for i in range(start, stop + 1):
+    for i in range(start, stop):
         html = get_html('http://edf.xmu.edu.cn/Donate?page=%d' % (i))
         if html != None:
             save_html(html.read().decode('utf-8'),
@@ -55,31 +56,42 @@ def download(start=1, stop=10):
             # logger.debug('Downloaded Page %d successfully.' % (i))
             logger.info('[%s]Downloaded Page %d successfully.' %
                         (threading.current_thread().getName(), i))
+            return True
+        else:
+            return False
 
 
-def work_fun(task_queue):
+def work_fun(task_queue, message_queue):
     logger = logging.getLogger(__file__)
     while True:
         i = task_queue.get()
-        download(i, i + 1)
+        if not download(i, i + 1):
+            message_queue.put(i)
+        # time.sleep(1)
         # logger.info('[%s]Downloaded Page %d successfully.' %
         #             (threading.current_thread().getName(), i))
         task_queue.task_done()
 
 
-def Multithread_download(total_page_number=10, max_workers=1):
+def Multithread_download(task_list, max_workers=1):
     logger = logging.getLogger(__file__)
     task_queue = queue.Queue()
-    for i in range(1, total_page_number + 1):
+    message_queue = queue.Queue()
+    for i in task_list:
         task_queue.put(i)
     for i in range(max_workers):
-        thread = threading.Thread(target=work_fun, args=(task_queue, ))
+        thread = threading.Thread(target=work_fun, args=(task_queue, message_queue, ))
         thread.daemon = True
         thread.start()
     task_queue.join()
     logger.info('Download completed.')
-
+    error_id = [message_queue.get() for i in range(message_queue.qsize())]
+    logger.info('Error id : %s'%(str(error_id)))
+    return error_id
 
 if __name__ == '__main__':
     init_logger()
-    Multithread_download(total_page_number=2119, max_workers=4)
+    task_list = list(range(2001, 2120))
+    while task_list:
+        task_list = Multithread_download(task_list, max_workers=2)
+    
